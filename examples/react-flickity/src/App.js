67e2f7158222d1a34982a7985d30c8c0a0f4e2b5
theolampert <theolampert@gmail.com>
2017-10-31 12:32:25 +0100

    Ignore example tests

--- examples/react-flickity/src/App.js
+++ examples/react-flickity/src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Flickity from 'react-flickity-component/src/index';
+import Flickity from 'react-flickity-component';
 
 class App extends Component {
   render() {
-
-    const flickityOptions = {
-      initialIndex: 1
-    }
 
     return (
       <div className="App">
         <Flickity
           className={'carousel'}
-          elementType={'div'}
-          options={flickityOptions}
-          disableImagesLoaded={false}
         >
           <img alt="example" src="http://via.placeholder.com/600x350"/>
           <img alt="example" src="http://via.placeholder.com/600x350"/>
